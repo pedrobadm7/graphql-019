31de3a584eaee5c1f97e05a6dbdc5a7009f0b18d
Pedro Barros <pedrobars7a@gmail.com>
2022-09-21 21:16:29 -0300

    é isso aí

---
+++ README.md
@@ -0,0 +1,8 @@
+Quais problemas o GraphQL resolve?
+  - Overfetching;
+  - Underfetching;
+
+Dificuldades: 
+  - Trabalhar com cache;
+  - Erros que o backend retorna é mais difícil de tratar (Em graphQL todas as chamadas HTTP retornam 200);
+  
---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import { gql, useQuery } from '@apollo/client'
+import { NewUserForm } from './components/NewUserForm';
+
+type User = {
+  id: string;
+  name: string;
+}
+
+export const GET_USER = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`
+
+function App() {
+  const { data, loading } = useQuery(GET_USER);
+  
+  console.log(data);
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <div>
+       <ul>
+      {data?.users.map((user: User) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <NewUserForm />
+   </div>
+  )
+}
+
+export default App
